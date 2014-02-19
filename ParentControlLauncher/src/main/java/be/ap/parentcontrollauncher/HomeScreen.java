f2ccb954583fce2c8bc7e29e4d1898feee541cd7
Wouter <wouterceulemans1@hotmail.com>
2014-02-19 14:43:39 +0100

    Added basic project

---
+++ ParentControlLauncher/src/main/java/be/ap/parentcontrollauncher/HomeScreen.java
@@ -0,0 +1,19 @@
+package be.ap.parentcontrollauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class HomeScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
