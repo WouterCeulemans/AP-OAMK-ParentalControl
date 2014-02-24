c9e834893409ad995098a3b5775422f3206c701e
nick wuyts <Wuyts.N@hotmail.com>
2014-02-24 11:59:59 +0100

    Added SMS App Reorganized Project folder

---
+++ SMSDemo2/app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.gsm.SmsManager;
+import android.telephony.gsm.SmsMessage;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity
+{
+    /**called when the activity first created*/
+    Button sendSMS;
+    EditText msgTxt;
+    EditText numTxt;
+    IntentFilter intentFilter;
+
+    private BroadcastReceiver intentReceiver = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            // display the message in the textview
+            TextView inTxt = (TextView) findViewById(R.id.textMsg);
+            inTxt.setText(intent.getExtras().getString("sms"));
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //intent to filter for SMS messages
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("SMS_RECEIVED_ACTION");
+
+
+        sendSMS = (Button) findViewById(R.id.sendBtn);
+        msgTxt =(EditText) findViewById(R.id.MessageTxt);
+        numTxt = (EditText) findViewById(R.id.PhoneTxt);
+        sendSMS.setOnClickListener
+                (
+                        new View.OnClickListener()
+                            {
+                                @Override
+                        public void onClick(View v)
+                                {
+                                    String myMsg        = msgTxt.getText().toString();
+                                    String theNumber    = numTxt.getText().toString();
+                                    sendMsg(theNumber, myMsg);
+                                }
+                            }
+                );
+
+
+    }
+
+    protected void sendMsg(String theNumber, String myMsg)
+    {
+        String SENT         = "Message Sent";
+        String DELIVERED    = "Message Delivered";
+
+        PendingIntent sentPI        =   PendingIntent.getBroadcast(this, 0  , new Intent(SENT)      , 0);
+        PendingIntent deliveredPI   =   PendingIntent.getBroadcast(this, 0  , new Intent(DELIVERED) , 0);
+
+        registerReceiver(new BroadcastReceiver()
+        {
+            public void onReceive(Context arg0, Intent arg1)
+            {
+                switch(getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(MainActivity.this, "SMS sent", Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic Failure", Toast.LENGTH_LONG).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(),"No Service", Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        registerReceiver(new BroadcastReceiver() {
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered", Toast.LENGTH_LONG).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), " SMS not delivered", Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        //sms.sendTextMessage(destinationAddress, scAddress, text, sentIntent, deliveryIntent);
+        sms.sendTextMessage(theNumber, null , myMsg, sentPI, deliveredPI);
+    }
+    @Override
+    protected void onResume()
+    {
+        //register the receiver
+        registerReceiver(intentReceiver, intentFilter);
+        super.onResume();
+    }
+    @Override
+    protected void onPause()
+    {
+        //unregister the receiver
+        unregisterReceiver(intentReceiver);
+        super.onPause();
+    }
+}
---
+++ SMSDemo2/app/src/main/java/com/example/app/SMSReceiver.java
@@ -0,0 +1,47 @@
+package com.example.app;
+
+/**
+ * Created by nick on 19/02/14.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+public class SMSReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        //get message passed in
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] messages = null;
+        String str = "" ;
+        if (bundle != null)
+        {
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            messages = new SmsMessage[pdus.length];
+            for (int i = 0; i < messages.length; i++)
+            {
+                messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                str +=  messages[i].getOriginatingAddress() + " Says" ;
+                str += " : ";
+                str += messages[i].getMessageBody().toString();
+                str += " \n " ;
+            }
+            //display message
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+
+            // send a broadcast intent to update the SMSreceived in a textview
+            Intent broadcastIntent = new Intent();
+            broadcastIntent.setAction("SMS_RECEIVED_ACTION");
+            broadcastIntent.putExtra("sms", str);
+            context.sendBroadcast(broadcastIntent);
+        }
+    }
+}
