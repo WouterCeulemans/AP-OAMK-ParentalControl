6cb513ffc7d9d522ce068c393817d05f1ad24893
nick wuyts <Wuyts.N@hotmail.com>
2014-04-15 14:32:36 +0200

    save btn added
    layout adjustment
    
    fake privacy policies implemented
    
    settingspage optimized
    
    ....

--- Web Center/Settings.php
+++ Web Center/Settings.php
@@ -8,24 +8,37 @@
 
 <body>
 
-
-<h1 id="Header1">Settings Menu</h1>
+<div id="divWrapper">
+	<div id="divHeader">
+    <img src="Images/Header.png" width="500" height="200"/>
+    </div>
+    </div>
 <div id="divHome">
-	<h3 id="Header3">MM</h3>
+
+
+
+    <div id="divLinks">
+    	<div id="divNav">
+        	<ul>
+            	<li><a href="Index.html" 					>Home</a></li>
+                <li><a href="Settings.html"class="current"	>Settings</a></li>
+                <li><a href="Spybot.html"					>Spybot</a></li>
+                <li><a href="Tracker.html"					>Tracker</a></li>
+            </ul>
+    	</div>
+    </div>
 		<h5 id="Header5">Messaging Management</h5>
 
 <form method="post" action="<?=$_SERVER['PHP_SELF'];?>">
 	<table id="tabs">
     	<tr><td>Amount of texts: </td><td><input type="text" name="text_message_limit"/></td></tr>
     	<tr><td>Block Number:</td><td><input type="text" name="text_num_lock"/></td></tr>
-       	<tr><td>Block Name:</td><td><input type="text" name="text_name_lock"/></td></tr>
-        
+       	<tr><td>Block Name:</td><td><input type="text" name="text_name_lock"/></td></tr>        
         <tr><td>Unblock Number:</td><td><input type="text" name="text_num_unlock"/></td></tr>
-        <tr><td height="32">Unblock Name:</td><td><input type="text" name="text_name_unlock"/></td></tr>
+        <tr><td>Unblock Name:</td><td><input type="text" name="text_name_unlock"/></td></tr>
 
     </table>
 </form>
-<h3 id="Header3">CM</h3>
 	<h5 id="Header5">Call Management</h5>
 
 <form method="post" action="<?=$_SERVER['PHP_SELF'];?>">
@@ -39,18 +52,22 @@
 
     </table>
 </form>
-<h3 id="Header3">AM</h3>
 	<h5 id="Header5">Application Management</h5>
 
 <form method="post" action="<?=$_SERVER['PHP_SELF'];?>">
 	<table id="tabs">
-    	<tr><td>Lock phone at:</td><td><input type="time" name="app_lock_phone"/></td></tr>
-    	<tr><td>Unlock phone at:</td><td><input type="time" name="app_unlock_phone"/></td></tr>
+    	<tr><td>Lock phone at:</td><td><input type="text" name="app_lock_phone"/></td></tr>
+    	<tr><td>Unlock phone at:</td><td><input type="text" name="app_unlock_phone"/></td></tr>
        	<tr><td>Lock App (name):</td><td><input type="text" name="app_lock_app"/></td></tr>
         <tr><td>Unlock App(name):</td><td><input type="text" name="app_unlock_app"/></td></tr>
-
-    </table>
-    
+    </table>    
+</form>
+<form method="post" action="<?=$_SERVER['PHP_SELF'];?>">
+<table>
+	<tr><td>
+    <button onClick="btnSave" id="btnOnclick">Save</button>
+    </td></tr>
+</table>
 </form>
 </div>
 </body>
