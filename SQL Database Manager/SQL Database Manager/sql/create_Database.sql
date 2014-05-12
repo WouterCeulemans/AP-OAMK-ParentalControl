93842dd5707bf82c45096265b71c984db0ff66f9
RobertSmits <robertsmits@hotmail.be>
2014-05-12 20:58:04 +0200

    Database fix

--- SQL Database Manager/SQL Database Manager/sql/create_Database.sql
+++ SQL Database Manager/SQL Database Manager/sql/create_Database.sql
@@ -1,49 +1,47 @@
 CREATE TABLE `apps` (
-  `ID`           varchar(20) NOT NULL,
-  `PackageName`  varchar(50) NOT NULL,
-  `Name`         varchar(50) NOT NULL,
-  `Visible`      int         NOT NULL,
+  `ID`          varchar(20) NOT NULL,
+  `PackageName` varchar(50) NOT NULL,
+  `Name`        varchar(50) NOT NULL,
+  `Visible`     int         NOT NULL,
   PRIMARY KEY (`ID`,`PackageName`)
 );
 
 CREATE TABLE `contacts` (
-  `ID`           varchar(20) NOT NULL,
-  `Contact_ID`   int         NOT NULL,
-  `SurName`      varchar(20) NOT NULL,
-  `Name`         varchar(20) NOT NULL,
-  `Number`       varchar(13) NOT NULL,
-  `TxtAmount`    int,
-  `TxtMax`       int,
-  `CallAmount`   int,
-  `CallMax`      int,
-  `Blocked`      tinyint(1)  NOT NULL DEFAULT 0,
+  `ID`          varchar(20) NOT NULL,
+  `Contact_ID`  int         NOT NULL,
+  `SurName`     varchar(20) NOT NULL,
+  `Name`        varchar(20) NOT NULL,
+  `Number`      varchar(13) NOT NULL,
+  `TxtAmount`   int,
+  `TxtMax`      int,
+  `CallAmount`  int,
+  `CallMax`     int,
+  `Blocked`     tinyint(1)  NOT NULL DEFAULT 0,
   PRIMARY KEY (`ID`,`Contact_ID`)
 );
 
 CREATE TABLE `coordinates` (
-  `ID`           varchar(20) NOT NULL,
-  `POS_ID`       int         NOT NULL,
-  `LATITUDE`     double      NOT NULL,
-  `LONGITUDE`    double      NOT NULL,
+  `ID`          varchar(20) NOT NULL,
+  `POS_ID`      int         NOT NULL,
+  `LATITUDE`    double      NOT NULL,
+  `LONGITUDE`   double      NOT NULL,
   PRIMARY KEY (`ID`,`POS_ID`)
 );
 
 CREATE TABLE `devices` (
-  `ID`           varchar(20) NOT NULL,
-  `Name`         varchar(20),
-  `User_ID`      int,
+  `ID`          varchar(20) NOT NULL,
+  `Name`        varchar(20),
+  `User_ID`     int,
   PRIMARY KEY (`ID`)
 );
 
 CREATE TABLE `users` (
-  `ID`           int         NOT NULL AUTO_INCREMENT,
-  `UserName`     varchar(20) NOT NULL,
-  `Pass`         varchar(40) NOT NULL,
-  `Email`        varchar(50) NOT NULL,
-  `Name`         varchar(20) NOT NULL,
-  `LastName`     varchar(20) NOT NULL,
-  `Theme`   	 varchar(5)  NOT NULL DEFAULT `light`,
-  `ResetHash`    varchar(40),
-  `ResetPending` tinyint(1)  NOT NULL DEFAULT `0`,
+  `ID`          int         NOT NULL AUTO_INCREMENT,
+  `UserName`    varchar(20) NOT NULL,
+  `Pass`        varchar(50) NOT NULL,
+  `Email`       varchar(50) NOT NULL,
+  `Name`        varchar(20) NOT NULL,
+  `LastName`    varchar(20) NOT NULL,
+  `Theme`   	varchar(5)  NOT NULL DEFAULT "light",
   PRIMARY KEY (`ID`)
 );
